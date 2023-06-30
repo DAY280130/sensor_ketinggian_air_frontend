a806412de76cb44c7537246cceb50f188a16cc33
DAY280130 <dimas.280130@gmail.com>
2023-06-30 13:08:55 +0700

    config edit for gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
+
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-});
+    base: '/',
+  }
+
+  if(command !== 'serve') {
+    config.base = '/sensor_ketinggian_air_frontend/'
+  }
+
+  return config
+})
