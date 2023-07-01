c6727ea8cddfa7332f5c5803310965fa37ab06af
DAY280130 <dimas.280130@gmail.com>
2023-07-01 11:51:08 +0700

    api ip control done

--- src/components/LeftPage.tsx
+++ src/components/LeftPage.tsx
@@ -1,11 +1,53 @@
+import { dataSchema } from "@/lib/schema";
 import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useReadLocalStorage } from "usehooks-ts";
+import * as z from "zod";
+
+const waterMonitorSchema = dataSchema.pick({
+  waterLevel: true,
+  waterStatus: true,
+});
+type WaterMonitorDatas = z.infer<typeof waterMonitorSchema>;
+
+const levelBreakpointSchema = dataSchema.pick({
+  level1Breakpoint: true,
+  level2Breakpoint: true,
+  level3Breakpoint: true,
+});
+type LevelBreakpoints = z.infer<typeof levelBreakpointSchema>;
 
 const LeftPage: React.FC = () => {
-  const waterLevel = 81;
-  const level1Breakpoint = 75.0;
-  const level2Breakpoint = 80.0;
-  const level3Breakpoint = 90.0;
-  const waterStatus = waterLevel >= level3Breakpoint ? "dangerous" : waterLevel >= level2Breakpoint ? "be warned" : waterLevel >= level1Breakpoint ? "safe" : "low";
+  const apiBaseUrl = useReadLocalStorage<string>("api-url");
+  const { data } = useQuery({
+    queryKey: ["left-page"],
+    queryFn: async () => {
+      const waterMonitorDatas = await (await axios.get<WaterMonitorDatas>(`${apiBaseUrl}/water`)).data;
+
+      const levelBreakpoints = await (await axios.get<LevelBreakpoints>(`${apiBaseUrl}/level`)).data;
+
+      return {
+        waterLevel: waterMonitorDatas.waterLevel,
+        waterStatus: waterMonitorDatas.waterStatus,
+        level1Breakpoint: levelBreakpoints.level1Breakpoint,
+        level2Breakpoint: levelBreakpoints.level2Breakpoint,
+        level3Breakpoint: levelBreakpoints.level3Breakpoint,
+      };
+    },
+    initialData: {
+      waterLevel: 0,
+      waterStatus: "unchecked",
+      level1Breakpoint: 0,
+      level2Breakpoint: 0,
+      level3Breakpoint: 0,
+    },
+    refetchIntervalInBackground: true,
+    refetchInterval: 1000,
+  });
+
+  const { level1Breakpoint, level2Breakpoint, level3Breakpoint, waterLevel, waterStatus } = data;
+
   return (
     <div className="relative flex h-[75vh] w-[75vw] flex-col justify-end rounded-b-xl border-4 border-foreground border-t-transparent md:w-[30vw]">
       <svg
@@ -15,20 +57,40 @@ const LeftPage: React.FC = () => {
         xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28"
         preserveAspectRatio="none"
-        shape-rendering="auto"
+        shapeRendering="auto"
       >
         <defs>
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
         <g>
-          <use className="animate-wave fill-blue-300 [animation-delay:-5s] [animation-duration:20s]" xlinkHref="#gentle-wave" x="48" y="0" />
-          <use className="animate-wave fill-blue-300/70 [animation-delay:-2s] [animation-duration:7s]" xlinkHref="#gentle-wave" x="48" y="0" />
-          <use className="animate-wave fill-blue-300/50 [animation-delay:-3s] [animation-duration:10s]" xlinkHref="#gentle-wave" x="48" y="0" />
-          <use className="animate-wave fill-blue-300/30 [animation-delay:-4s] [animation-duration:13s]" xlinkHref="#gentle-wave" x="48" y="0" />
+          <use
+            className="animate-wave fill-blue-300 transition-all [animation-delay:-5s] [animation-duration:20s] [transition-duration:1s]"
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+          />
+          <use
+            className="animate-wave fill-blue-300/70 transition-all [transition-duration:1s] [animation-delay:-2s] [animation-duration:7s]"
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+          />
+          <use
+            className="animate-wave fill-blue-300/50 transition-all [transition-duration:1s] [animation-delay:-3s] [animation-duration:10s]"
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+          />
+          <use
+            className="animate-wave fill-blue-300/30 transition-all [transition-duration:1s] [animation-delay:-4s] [animation-duration:13s]"
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+          />
         </g>
       </svg>
-      <div className="w-full rounded-b-lg bg-blue-300" style={{ height: `${waterLevel >= 5 ? waterLevel - 5.0 : 0.0}%` }}></div>
-      <div className="absolute left-0 flex h-1 w-[125%] items-center" style={{ bottom: `${level3Breakpoint}%` }}>
+      <div className="w-full rounded-b-lg bg-blue-300 transition-all [transition-duration:1s]" style={{ height: `${waterLevel >= 5 ? waterLevel - 5.0 : 0.0}%` }}></div>
+      <div className="absolute left-0 flex h-1 w-[125%] items-center transition-all duration-700" style={{ bottom: `${level3Breakpoint}%` }}>
         <hr className="h-1 w-[80%] bg-destructive" />
         <span
           className={cn(
@@ -39,7 +101,7 @@ const LeftPage: React.FC = () => {
           {level3Breakpoint}%
         </span>
       </div>
-      <div className="absolute left-0 flex h-1 w-[125%] items-center" style={{ bottom: `${level2Breakpoint}%` }}>
+      <div className="absolute left-0 flex h-1 w-[125%] items-center transition-all duration-700" style={{ bottom: `${level2Breakpoint}%` }}>
         <hr className="h-1 w-[80%] bg-warning" />
         <span
           className={cn(
@@ -50,7 +112,7 @@ const LeftPage: React.FC = () => {
           {level2Breakpoint}%
         </span>
       </div>
-      <div className="absolute left-0 flex h-1 w-[125%] items-center" style={{ bottom: `${level1Breakpoint}%` }}>
+      <div className="absolute left-0 flex h-1 w-[125%] items-center transition-all duration-700" style={{ bottom: `${level1Breakpoint}%` }}>
         <hr className="h-1 w-[80%] bg-safezone" />
         <span
           className={cn(
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,83 +1,84 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Moon, Sun, Laptop2 } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Moon, Sun, Laptop2, Link2 } from "lucide-react";
 import React from "react";
-
-type ThemeList = "light" | "dark";
+import { useLocalStorage, useTernaryDarkMode } from "usehooks-ts";
 
 const TopBar: React.FC = () => {
-  const [systemTheme, setSystemTheme] = React.useState<ThemeList>(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  });
-  const [theme, setTheme] = React.useState<ThemeList | "system">(!localStorage.theme ? "system" : localStorage.theme);
+  const { isDarkMode, setTernaryDarkMode } = useTernaryDarkMode();
+  const [apiBaseUrl, setApiBaseUrl] = useLocalStorage("api-url", "http://localhost:3000");
+  const [dialogOpen, setDialogOpen] = React.useState(false);
 
   React.useEffect(() => {
-    if (theme === "dark" || theme === "light") {
-      localStorage.setItem("theme", theme);
-      if (theme === "dark") {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
     } else {
-      localStorage.removeItem("theme");
-      if (systemTheme === "dark") {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
+      document.documentElement.classList.remove("dark");
     }
-  }, [theme, systemTheme]);
-
-  React.useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    mediaQuery.addEventListener("change", (e) => {
-      if (e.matches) {
-        setSystemTheme("dark");
-      } else {
-        setSystemTheme("light");
-      }
-    });
-  }, []);
+  }, [isDarkMode]);
 
   return (
     <div className="flex justify-between bg-primary px-5 py-3 text-primary-foreground">
-      <span className="text-3xl font-bold ">Sensor Ketinggian Air</span>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button className="hover:bg-accent/5 hover:text-accent focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0" variant={"ghost"} size={"sm"}>
-            {theme === "dark" ? (
-              <Moon className="h-6 w-6" />
-            ) : theme === "light" ? (
-              <Sun className="h-6 w-6" />
-            ) : systemTheme === "dark" ? (
-              <Moon className="h-6 w-6" />
-            ) : (
-              <Sun className="h-6 w-6" />
-            )}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Choose Theme</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTheme("system")}>
-            <Laptop2 className="h-5 w-5" />
-            System
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTheme("light")}>
-            <Sun className="h-5 w-5" />
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTheme("dark")}>
-            <Moon className="h-5 w-5" />
-            Dark
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <span className="text-3xl font-bold ">Water Level Sensor</span>
+      <span className="flex gap-2">
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+          <DialogTrigger asChild>
+            <Button className="hover:bg-accent/5 hover:text-accent focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0" variant={"ghost"} size={"sm"}>
+              <Link2 className="h-6 w-6" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-sm">
+            <DialogHeader>
+              <DialogTitle>Microcontroller IP Address</DialogTitle>
+              <DialogDescription>Enter the Microcontroller's IP Address here to start control the water level sensor</DialogDescription>
+            </DialogHeader>
+            <div className="flex items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                IP
+              </Label>
+              <Input id="api-url-input" placeholder="http://1.1.1.1:80" defaultValue={apiBaseUrl} />
+            </div>
+            <DialogFooter>
+              <Button
+                type="button"
+                onClick={() => {
+                  const input = document.getElementById("api-url-input") as HTMLInputElement;
+                  setApiBaseUrl(input.value);
+                  setDialogOpen(false);
+                }}
+              >
+                Save
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="hover:bg-accent/5 hover:text-accent focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0" variant={"ghost"} size={"sm"}>
+              {isDarkMode ? <Moon className="h-6 w-6" /> : <Sun className="h-6 w-6" />}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Choose Theme</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTernaryDarkMode("system")}>
+              <Laptop2 className="h-5 w-5" />
+              System
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTernaryDarkMode("light")}>
+              <Sun className="h-5 w-5" />
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex items-center gap-2" onClick={() => setTernaryDarkMode("dark")}>
+              <Moon className="h-5 w-5" />
+              Dark
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </span>
     </div>
   );
 };
