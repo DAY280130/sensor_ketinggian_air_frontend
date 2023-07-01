6b11f14f82c6c3adba459782be4066e4dc007db6
DAY280130 <dimas.280130@gmail.com>
2023-07-01 14:56:42 +0700

    fix minor pixel tearing

--- src/components/LeftPage.tsx
+++ src/components/LeftPage.tsx
@@ -48,8 +48,8 @@ const LeftPage: React.FC = () => {
   return (
     <div className="relative flex h-[75vh] w-[75vw] flex-col justify-end rounded-b-xl border-4 border-foreground border-t-transparent md:w-[30vw]">
       <svg
-        className={`relative w-full ${waterLevel <= 5 && "rounded-b-lg"}`}
-        style={{ height: `${waterLevel <= 5 ? waterLevel : 5}%` }}
+        className={`relative w-full transition-all duration-1000 ${waterLevel <= 5 && "rounded-b-lg"}`}
+        style={{ height: `${waterStatus === "unchecked" ? 0 : waterLevel <= 5 ? waterLevel : 5}%` }}
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28"
@@ -60,33 +60,16 @@ const LeftPage: React.FC = () => {
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
         <g>
-          <use
-            className="animate-wave fill-blue-300 transition-all [animation-delay:-5s] [animation-duration:20s] [transition-duration:1s]"
-            xlinkHref="#gentle-wave"
-            x="48"
-            y="0"
-          />
-          <use
-            className="animate-wave fill-blue-300/70 transition-all [transition-duration:1s] [animation-delay:-2s] [animation-duration:7s]"
-            xlinkHref="#gentle-wave"
-            x="48"
-            y="0"
-          />
-          <use
-            className="animate-wave fill-blue-300/50 transition-all [transition-duration:1s] [animation-delay:-3s] [animation-duration:10s]"
-            xlinkHref="#gentle-wave"
-            x="48"
-            y="0"
-          />
-          <use
-            className="animate-wave fill-blue-300/30 transition-all [transition-duration:1s] [animation-delay:-4s] [animation-duration:13s]"
-            xlinkHref="#gentle-wave"
-            x="48"
-            y="0"
-          />
+          <use className="animate-wave fill-blue-300 [animation-delay:-5s] [animation-duration:20s]" xlinkHref="#gentle-wave" x="48" y="0" />
+          <use className="animate-wave fill-blue-300/70 [animation-delay:-2s] [animation-duration:7s]" xlinkHref="#gentle-wave" x="48" y="0" />
+          <use className="animate-wave fill-blue-300/50 [animation-delay:-3s] [animation-duration:10s]" xlinkHref="#gentle-wave" x="48" y="0" />
+          <use className="animate-wave fill-blue-300/30 [animation-delay:-4s] [animation-duration:13s]" xlinkHref="#gentle-wave" x="48" y="0" />
         </g>
       </svg>
-      <div className="w-full rounded-b-lg bg-blue-300 transition-all [transition-duration:1s]" style={{ height: `${waterLevel >= 5 ? waterLevel - 5.0 : 0.0}%` }}></div>
+      <div
+        className="-mt-[0.1px] w-full rounded-b-lg bg-blue-300 transition-all duration-500"
+        style={{ height: `${waterStatus === "unchecked" ? 0 : waterLevel >= 5 ? waterLevel - 5.0 : 0.0}%` }}
+      ></div>
       <div className="absolute left-0 flex h-1 w-[125%] items-center transition-all duration-700" style={{ bottom: `${level3Breakpoint}%` }}>
         <hr className="h-1 w-[80%] bg-destructive" />
         <span
